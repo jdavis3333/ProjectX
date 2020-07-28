8b35722187991d43109ac24cea07fb680f4f795a
Ayla Dillis <aayladillis@gmail.com>
2020-07-27 20:06:44 -0700

    adding public folder

--- schema.sql
+++ schema.sql
@@ -1,4 +1,4 @@
 -- Drops the blogger if it exists currently --
-DROP DATABASE IF EXISTS projectX;
+DROP DATABASE IF EXISTS maze_runner;
 -- Creates the "blogger" database --
-CREATE DATABASE projectX;
+CREATE DATABASE maze_runner;
