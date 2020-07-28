ac3d1e957988c249e0f11b57953b53733a326673
Ayla Dillis <aayladillis@gmail.com>
2020-07-28 13:45:25 -0700

    materialize

--- schema.sql
+++ schema.sql
@@ -1,4 +1,4 @@
 -- Drops the blogger if it exists currently --
-DROP DATABASE IF EXISTS maze_runner;
+DROP DATABASE IF EXISTS maze_runner_db;
 -- Creates the "blogger" database --
-CREATE DATABASE maze_runner;
+CREATE DATABASE maze_runner_db;
