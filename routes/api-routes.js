e54adbeb68d8ac380c7881c5e597c14bcc724bd0
jdavis3333 <jdavis3333@gmail.com>
2020-07-28 20:02:57 -0700

    routes updated

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,11 +3,24 @@
 var db = require("../models")
 
 
-
+//Routes
+//Get route for retrieving a story and choices
 app.get("/api/stories_id/:id", function(req, res) {
-    
-})
+    db.Stories.findStory({
+        where: {
+            id: req.params.id
+        },
+        include: [db.Choices] //need to update/verify in models
+    }).then(function(dbStory){
+        res.json(dbStory);
+    });
+});
 
-api/
 
-app.post("api/")
+//Post route for saving player's name
+app.post("api/stories", function(req, res) {
+    db.Stories.create(req.body).then(function(dbStory) {
+        res.json(dbStory);
+    });
+});
+
