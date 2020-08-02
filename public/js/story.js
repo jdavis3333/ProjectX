19d8417fe178bf7d32bc47aaddfbbca796d23a42
jdavis3333 <jdavis3333@gmail.com>
2020-08-02 14:49:10 -0700

    renaming images

--- public/js/story.js
+++ public/js/story.js
@@ -2,6 +2,8 @@
 var btn0 = document.querySelector('#btn0')
 var btn1 = document.querySelector('#btn1')
 
+var imageArr = []
+
 btn0.addEventListener('click', () =>{
 document.body.style.backgroundImage = "url('')"
 })
